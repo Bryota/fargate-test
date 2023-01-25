a8fd0c3bbb02a622449ed173c9ad34f8ff7252c6
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2023-01-25 22:29:00 +0900

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
     def index
         logger.info "============================================================"
         logger.info "home"
-        render json: {status: 200}, status: 200
+        render json: {status: 200, test: 'hahaha'}, status: 200
     end
 end
